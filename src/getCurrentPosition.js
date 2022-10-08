8b8a6f7a2ed40834811a2e5be111635fc88d6588
joseSoberon <jose.ant.soberon@gmail.com>
2022-10-08 00:54:14 -0500

    Make getCurrentPosition() get all weather data

--- src/getCurrentPosition.js
+++ src/getCurrentPosition.js
@@ -1,15 +1,27 @@
 export default function getCurrentPosition() {
-    const id = navigator.geolocation.getCurrentPosition(succesCallback, errorCallback);
+    const id = navigator.geolocation.getCurrentPosition(succesCallback, errorCallback)
 }
 
 const succesCallback  = (position) => {
-    let latitude = document.getElementById('latitude');
-    latitude.textContent = position.coords.longitude;
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
 
-    let longitude = document.getElementById('longitude')
-    longitude.textContent = position.coords.latitude;
+    let apiKey = '8a1dfe2baf222a773618cc95daa29f42';
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+    fetch(url, {mode: 'cors'})
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(response) {
+            let city = document.getElementById("city");
+            city.textContent = response.name;
+
+            console.log(city);
+        })
+
 };
 
 const errorCallback = () => {
-    // TO DO : Set a random coords to display the weather when user dont allow position
+    // TO DO : Set a random coords to display the weather when user dont allow
 }
